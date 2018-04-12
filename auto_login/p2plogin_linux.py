e46be6777551b41dbf2648d95b1905e43ba0efa3
kroneyen <krone.huang@gmail.com>
2018-04-12 12:03:20 +0800

    modify next user waitting time
    modify next user waitting time

--- auto_login/p2plogin_linux.py
+++ auto_login/p2plogin_linux.py
@@ -12,17 +12,18 @@ import logging
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import random
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%m-%d %H:%M',
+                    datefmt='%Y-%m-%d %H:%M:%S',
                     filename='logging.log',
 		    filemode='a')
 
 
 logging.info(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
-myusername_list =["XXXXXX","XXXX"] 
+myusername_list =["XXXXXX","XXXXXX"] 
 mypassword_list =["XXXXXX","XXXXXX"]
 
 
@@ -51,35 +52,35 @@ for i in range(len(myusername_list)):
 
 	#logging.info("login botton is success")
 
-	time.sleep(1)
-
+	time.sleep(random.randrange(1, 5, 1))
 
 	## login user task
-
+        
 	web.get(url2)
-	#logging.info("login task page is success")
-	time.sleep(1)
-
-	## got redpackage 
-
+	time.sleep(random.randrange(1, 5, 1))
+	
+        ## got redpackage 
 	try: 
 	    link = WebDriverWait(web, 10).until(EC.element_to_be_clickable((By.XPATH, "//img[@alt='apply']")))
 	    link.click()
 	    logger = logging.getLogger(myusername)		
 	    logger.info("get redpackage is successed!!")
 
-
 	except:
-	       logger = logging.getLogger(myusername)	
-	       logger.info("link is not exist!!")
-
-
-	web.quit()
-	#print('user:',myusername, 'web quit is sucesses ')
-
+            logger = logging.getLogger(myusername)
+            logger.info("link is not exist!!")
 	
-	#print('wait 60 sec !!!')
-	time.sleep(180)
+	## close web 	
+	time.sleep(random.randrange(1, 10, 1))
+	web.quit()
 
+	## waiting next trun user
+	if i < (len(myusername_list)-1):	
+	   logging.info("waiting for next  user!!")
+	   time.sleep(random.randrange(60, 180, 10))
+	else :
+	   break
+        	
+
+logging.info("user all done!!")
 display.stop()
-
