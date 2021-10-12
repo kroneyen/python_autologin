ee884b4a70219c73526a6f3ebc33be73c2f1d086
kroneyen <krone.huang@gmail.com>
2021-10-12 19:08:37 +0800

    modify bug

--- auto_login/no_ip/no_ip_confirm.py
+++ auto_login/no_ip/no_ip_confirm.py
@@ -16,6 +16,7 @@ import logging
 import send_mail
 from bs4 import BeautifulSoup
 import re
+from selenium.webdriver.common.action_chains import ActionChains
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -26,7 +27,8 @@ logging.basicConfig(level=logging.INFO,
 logging.info(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
 
 link_url = 'https://www.noip.com/login'
-hostname_url = 'https://my.noip.com/#!/dynamic-dns'
+hostname_url = 'https://my.noip.com/dynamic-dns'
+#hostname_url = 'https://www.noip.com/members/dns/'
 
 
 ## Usage Virtual Dispaly
@@ -67,29 +69,29 @@ def get_redis_data():
 
     return myusername_list , mypassword_list ,domain_list
 
-
-
 def get_option_num(soup,tb_id,_domain) :
     table_id = soup.find('table',{'class':re.compile(tb_id)})  ## find table_id                                   
-    _tr_class_list = []              
-    row_num =1                                                                            
+    _tr_class_list = []
+    row_num =1
     ### find striped-row                                                                                                            
-    for _t_striped in table_id.find_all('tr',{'class':re.compile('table-striped-row')}): 
-        _tr_class_list.append(_t_striped)                                                                       
-    
-    ### find domain && Confirm botton index 
-    for _tr_class in _tr_class_list:                                                                            
-      _td = _tr_class.find('a', {'class': re.compile('text-info cursor-pointer')})  
-      if _td.string == _domain:                                                                                 
-             break                                                                                              
-      else :                                                                                                    
-          row_num = row_num + 1                                                                                 
+    for _t_striped in table_id.find_all('tr',{'class':re.compile('table-striped-row')}):
+        _tr_class_list.append(_t_striped)
 
-    return row_num     
+    ### find domain && Confirm botton index 
+    for _tr_class in _tr_class_list:
+      #_td = _tr_class.find('a', {'class': re.compile('text-info cursor-pointer')})
+      _td = _tr_class.find('a', {'class': re.compile('link-info cursor-pointer')})
+      if _td.string == _domain:
+             break
+      else :
+          row_num = row_num + 1
+
+    return row_num 
+
 
 
 ## get user & pwd 
-#myusername_list , mypassword_list , domain_list = get_config() ## get loging user && pwd 
+## myusername_list , mypassword_list , domain_list = get_config() ## get loging user && pwd 
 myusername_list , mypassword_list , domain_list = get_redis_data() ## get loging user && pwd 
 
     
@@ -101,46 +103,64 @@ for num in range(len(myusername_list)):
     web = webdriver.Chrome('/usr/local/bin/chromedriver') ## for cron path
     ##web = webdriver.Chrome()
     web.get(link_url) 
-    time.sleep(random.randrange(1, 5, 1))
+    time.sleep(random.randrange(3, 5, 1))
     
     ### find loging_from 
-    loging_from = web.find_element_by_id('clogs')
-    loging_from.find_element_by_name("username").send_keys(myusername)
-    time.sleep(random.randrange(1, 5, 1))
-    loging_from.find_element_by_name("password").send_keys(mypassword)
-    time.sleep(random.randrange(1, 5, 1))
-    loging_from.find_element_by_name("Login").submit()
-    time.sleep(random.randrange(1, 5, 1))
-     
-    logger = logging.getLogger(myusername)
-    logger.info("login botton is success")
-    time.sleep(random.randrange(1, 5, 1))
-
+    loginForm = web.find_element_by_id('clogs')
+    fromname =loginForm.find_element_by_name("username")
+    frompwd = loginForm.find_element_by_name("password")
+    click_btn = loginForm.find_element_by_id("clogs-captcha-button")
+    time.sleep(random.randrange(5, 10, 1))    
+    
+    ActionChains(web).move_to_element(loginForm).send_keys_to_element(fromname, myusername).pause(random.randrange(1, 3, 1)).send_keys_to_element(frompwd, mypassword).pause(random.randrange(1, 3, 1)).pause(random.randrange(1, 3, 1)).click(click_btn).perform()
+    time.sleep(random.randrange(5, 10, 1))
+   
+    """ 
+    try:
+        ActionChains(web).move_to_element(loginForm).send_keys_to_element(fromname, myusername).send_keys_to_element(frompwd, mypassword).click(click_btn).perform()    
+        logger = logging.getLogger(myusername)
+        logger.info("login botton is success")
+        time.sleep(random.randrange(5, 10, 1))
+   
+    except : 
+        logger = logging.getLogger(myusername)
+        logger.info("login botton is failed")
+        time.sleep(random.randrange(5, 10, 1))
+        break
+    """
     #### web get expire hostname login
     web.get(hostname_url)
-    time.sleep(random.randrange(2, 5, 1))
+    time.sleep(random.randrange(5, 10, 1))
     logger = logging.getLogger(myusername)
     logger.info("loging account hostname page is successed!!")
-    soup = BeautifulSoup(web.page_source , "html.parser")
-    #host_tag = soup.find_all("a", class_="text-info cursor-pointer")
     
+    soup = BeautifulSoup(web.page_source , "html.parser")
+    #print('hostname_url:',soup)    
+
     ### domain confirm botton check 
     for domain_idx in domain_list :
         condi = ''
-        o_num =get_option_num(soup,'table no-margin-bv table-stack',domain_idx)
+        o_num  =get_option_num(soup,'table no-margin-bv table-stack',domain_idx)
         condi= '//*[@id="host-panel"]/table/tbody/tr['+ str(o_num) +']/td[5]/button[contains(.,"Confirm")]'
+        chk_down= '//*[@id="host-panel"]/table/tbody/tr['+ str(o_num) +']/td[5]/button[contains(.,"Modify")]'
+        logger = logging.getLogger(domain_idx)
         try :
              btn_confirm = WebDriverWait(web, 10).until(EC.element_to_be_clickable((By.XPATH,condi)))
-             #WebDriverWait(web, 10).until(EC.element_to_be_clickable((By.XPATH,"//button[contains(.,'Confirm')]")))
              btn_confirm.click()
-             #print('btn_confirm is successed')
-             logger = logging.getLogger(domain_idx)
-             logger.info("domain confirm is successed!!")
+             time.sleep(random.randrange(5, 10, 1))
+             try : ### After check Modify status is exiest
+                  WebDriverWait(web, 10).until(EC.presence_of_element_located((By.XPATH,chk_down)))
+                  logger.info("domain %s confirm is successed!!" % domain_idx)
 
-        except : 
-                logger = logging.getLogger(domain_idx)
-                logger.info("domain confirm is pass !!")
-        
+             except :
+               logger.info("domain $s confirm is failed!!" % domain_idx)
+
+        except :
+                WebDriverWait(web, 10).until(EC.presence_of_element_located((By.XPATH,chk_down)))
+                logger.info("domain %s confirm is pass !!" % domain_idx)
+
+
+
         ### wait next domain btn check 
         logging.info("waiting for next  domain!!")
         logging.info(" ")
